a2f70ba6b0bfd29b80936ac5b6f7ad1b46653de7
Crazybaud <antoine@fraticelli.me>
2016-11-20 12:41:17 +0100

    chat + home

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,31 @@
 angular.module('starter.controllers', [])
-
-.controller('DashCtrl', function($scope, $state) {
-    $scope.goToStatus = function() {
+.controller('DashCtrl', function($scope, $state, $http) {
+    $scope.sendStatus = function(value) {
+        $http({
+            method: 'POST',
+            url: 'http://posttestserver.com/post.php',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'},
+            data: { "id": "1", "date": "12/02/44", "alert": "orange"}
+        });
+    };
+    $scope.goToStatus = function(){
+        $scope.sendStatus();
         $state.go('tab.status');
     };
 })
+.controller('StatusCtrl', function($scope, $state, $http) {
+    $scope.tree = [
+        { 'question': 'Allez-vous moins bien qu\'aux urgences ?', 'oui': 1, 'non': 2, 'value':undefined, 'alert':undefined, 'type':'bool' },
+        { 'question': 'Quel est votre niveau de douleur de 1 à 10', 'oui': 2, 'non': 2, 'value':undefined, 'alert':undefined, 'type':'eva'},
+        { 'question': 'Votre abdomen est-il gonflé ?', 'oui': 3, 'non': 3, 'value':undefined, 'alert':undefined, 'type':'bool'},
+        { 'question': 'Avez-vous des vomissements ?', 'oui': 4, 'non': 6, 'value':undefined, 'alert':undefined, 'type':'bool'},
+        { 'question': 'Avez-vous réussi à prendre votre traitement ?', 'oui': 6, 'non': 5, 'value':undefined, 'alert':undefined, 'type':'bool'},
+        { 'question': 'Avez-vous réussi à boire ?', 'oui': 6, 'non': 6, 'value':undefined, 'alert':undefined, 'type':'bool'},
+        { 'question': 'Avez-vous une fièvre supérieur à 39° ?', 'oui': 7, 'non': 7, 'value':undefined, 'alert':undefined, 'type':'bool'},
+        { 'question': 'Merci, nous avons été informés.', 'oui': 0, 'non': 0, 'value':undefined, 'alert':undefined, 'type':'end'}
+    ];
+    $scope.treeindex = 0;
 
-.controller('StatusCtrl', function($scope, $state) {
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
